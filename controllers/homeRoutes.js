63498311c4abf83ef7cf77fc827d48d90c504128
Your Name <brodie@gruuvgroup.com>
2021-05-05 18:11:36 -0500

    computer updated & terminated

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
 
 router.get('/blog/:id', async (req, res) => {
   try {
-    const blogData = await blog.findByPk(req.params.id, {
+    const blogData = await Blog.findByPk(req.params.id, {
       include: [
         {
           model: User,
